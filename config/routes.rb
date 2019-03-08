4a6b3cda2c1b8dbb822da2bbd14b2e4c118d0848
Kaylin Bittner <kaylinbittner@gmail.com>
2019-03-08 14:34:22 +0100

    home page updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home', as: :home
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
